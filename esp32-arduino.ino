58a501f9932b4c30b46fccdd1d5f8e08a9da5840
Sharada Shehan <sharadashehan6@gmail.com>
2024-02-29 14:36:18 +0530

    timezone settings

--- esp32-arduino.ino
+++ esp32-arduino.ino
@@ -2,6 +2,7 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 #include <DHTesp.h>
+#include <WiFi.h>
 
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
@@ -18,13 +19,18 @@
 
 DHTesp dht;
 
+int utc_offset = 0;
+int dst_offset = 0;
+
 int milliTimenow = 0;
+int timeinit[3] = {0, 0, 0};
 int timenow[3] = {0, 0, 0};
 int buzzer_tones[6] = {262, 294, 330, 349, 392, 440};
 int buzzer_tone_count = 6;
 float humidity;
 float temperature;
 
+int current_alarm_option = 0;
 const int alarm_count = 3;
 int alarm_times[alarm_count][2] = {
   {0, 1},
@@ -33,6 +39,10 @@ int alarm_times[alarm_count][2] = {
 };
 bool alarm_ringing_finished[alarm_count] = {false, false, false};
 bool alarms_enabled = true;
+
+String menu_options[] = {"Set Time Zone", "Set Alarm", "Enable/Disable Alarms"};
+int menu_option_count = 3;
+int current_menu_option = 0;
 
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
@@ -55,15 +65,31 @@ void setup() {
   display.display();
   delay(2000);
 
+  WiFi.begin("Wokwi-GUEST", "", 6);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(250);
+    display.clearDisplay();
+    display.println("Connecting to WiFi...");
+  }
+
+  display.clearDisplay();
+  display.println("Connected to the WiFi network");
+
+  update_time();
+
+  display.display();
+  delay(2000);
+
   display.clearDisplay();
 
   display.setTextSize(1);
   display.setTextColor(SSD1306_WHITE);
   display.setCursor(0, 0);
-  display.println("Hello, world!");
+  display.println("Welcome to Medibox");
 
   display.display();
   delay(2000);
+
 }
 
 void loop() {
@@ -71,7 +97,10 @@ void loop() {
   if (alarms_enabled) {
     check_alarm_reached();
   }
-  delay(1000);
+  if (digitalRead(OK_BUTTON) == LOW) {
+    go_to_menu();
+  }
+  delay(500);
 }
 
 void print_text_line(String text, int row = 0, int column = 0, int text_size = 1) {
@@ -81,13 +110,155 @@ void print_text_line(String text, int row = 0, int column = 0, int text_size = 1
   display.display();
 }
 
+void show_menu() {
+  display.clearDisplay();
+  print_text_line("Menu", 0, 0, 2);
+  for (int i = 0; i < menu_option_count; i++) {
+    if (i == current_menu_option) {
+      print_text_line("-> " + menu_options[i], 20 + i * 10, 0);
+    } else {
+      print_text_line(menu_options[i], 20 + i * 10, 0);
+    }
+  }
+}
+
+void update_time() {
+  configTime(utc_offset * 3600, dst_offset * 3600, "pool.ntp.org");
+  struct tm timeinfo;
+  if (!getLocalTime(&timeinfo)) {
+    Serial.println("Failed to obtain time");
+    return;
+  }
+  char timeSecond[3], timeMinute[3], timeHour[3];
+  strftime(timeSecond, 3, "%S", &timeinfo);
+  strftime(timeMinute, 3, "%M", &timeinfo);
+  strftime(timeHour, 3, "%H", &timeinfo);
+  timeinit[0] = atoi(timeHour);
+  timeinit[1] = atoi(timeMinute);
+  timeinit[2] = atoi(timeSecond);
+  display.clearDisplay();
+}
+
+void display_time_zone(int utc_offset) {
+  display.clearDisplay();
+  if (utc_offset >= 0) {
+    print_text_line("Current Time Zone: UTC+" + String(utc_offset), 0, 0);
+  } else {
+    print_text_line("Current Time Zone: UTC" + String(utc_offset), 0, 0);
+  }
+}
+
+void set_time_zone() {
+  int temp_utc_offset = utc_offset;
+  display_time_zone(temp_utc_offset);
+  delay(200);
+  while (true) {
+    if (digitalRead(UP_BUTTON) == LOW) {
+      temp_utc_offset++;
+      if (temp_utc_offset > 14) {
+        temp_utc_offset = -12;
+      }
+      display_time_zone(temp_utc_offset);
+    } else if (digitalRead(DOWN_BUTTON) == LOW) {
+      temp_utc_offset--;
+      if (temp_utc_offset < -12) {
+        temp_utc_offset = 14;
+      }
+      display_time_zone(temp_utc_offset);
+    } else if (digitalRead(OK_BUTTON) == LOW) {
+      utc_offset = temp_utc_offset;
+      display.clearDisplay();
+      if (utc_offset >= 0) {
+        print_text_line("Time Zone set to UTC+" + String(utc_offset), 0, 0);
+      } else {
+        print_text_line("Time Zone set to UTC" + String(utc_offset), 0, 0);
+      }
+      delay(2000);
+      update_time();
+      break;
+    } else if (digitalRead(CANCEL_BUTTON) == LOW) {
+      break;
+    }
+    delay(200);
+  }
+  display.clearDisplay();
+  show_menu();
+  delay(200);
+}
+
+void display_alarm_menu() {
+  display.clearDisplay();
+  print_text_line("Select alarm to set", 0, 0);
+  for (int i = 0; i < alarm_count; i++) {
+    if (i == current_alarm_option) {
+      print_text_line("-> Alarm " + String(i + 1) + ": " + String(alarm_times[i][0]) + ":" + String(alarm_times[i][1]), 10 + i * 10, 0);
+    } else {
+      print_text_line("Alarm " + String(i + 1) + ": " + String(alarm_times[i][0]) + ":" + String(alarm_times[i][1]), 10 + i * 10, 0);
+    }
+  }
+}
+
+void set_alarm_time(int alarm_index) {
+  display.clearDisplay();
+  print_text_line("Set alarm " + String(alarm_index + 1) + " time", 0, 0);
+  delay(1000);
+}
+
+void set_alarm() {
+  while (true) {
+    if (digitalRead(UP_BUTTON) == LOW) {
+      current_alarm_option = (current_alarm_option - 1) % alarm_count;
+      if (current_alarm_option == -1) { current_alarm_option = alarm_count - 1; }
+      display_alarm_menu();
+    } else if (digitalRead(DOWN_BUTTON) == LOW) {
+      current_alarm_option = (current_alarm_option + 1) % alarm_count;
+      display_alarm_menu();
+    } else if (digitalRead(OK_BUTTON) == LOW) {
+      set_alarm_time(current_alarm_option);
+      display_alarm_menu();
+    } else if (digitalRead(CANCEL_BUTTON) == LOW) {
+      display.clearDisplay();
+      break;
+    }
+    delay(200);
+  } 
+}
+
+void go_to_menu() {
+  show_menu();
+  while (true) {
+    if (digitalRead(UP_BUTTON) == LOW) {
+      current_menu_option = (current_menu_option - 1) % menu_option_count;
+      if (current_menu_option == -1) { current_menu_option = menu_option_count - 1; }
+      show_menu();
+    } else if (digitalRead(DOWN_BUTTON) == LOW) {
+      current_menu_option = (current_menu_option + 1) % menu_option_count;
+      show_menu();
+    } else if (digitalRead(OK_BUTTON) == LOW) {
+      if (current_menu_option == 0) {
+        set_time_zone();
+      } else if (current_menu_option == 1) {
+        set_alarm();
+      } else if (current_menu_option == 2) {
+        display.clearDisplay();
+        print_text_line("enable/disable alarms");
+        delay(2000);
+      }
+    } else if (digitalRead(CANCEL_BUTTON) == LOW) {
+      display.clearDisplay();
+      return;
+    }
+  }
+}
+
+
 void update_time_and_temp() {
   humidity = dht.getHumidity();
   temperature = dht.getTemperature();
   milliTimenow = millis();
-  timenow[0] = (milliTimenow / 1000 / 60 / 60) % 24;
-  timenow[1] = (milliTimenow / 1000 / 60) % 60;
-  timenow[2] = (milliTimenow / 1000) % 60;
+  timenow[0] = timeinit[0] + (milliTimenow / 1000 / 60 / 60) % 24;
+  timenow[1] = timeinit[1] + (milliTimenow / 1000 / 60) % 60;
+  timenow[2] = timeinit[2] + (milliTimenow / 1000) % 60;
   display.clearDisplay();
   print_text_line("Time: " + String(timenow[0]) + ":" + String(timenow[1]) + ":" + String(timenow[2]), 0, 0);
   print_text_line("Temperature: " + String(temperature) + "C", 10, 0);
